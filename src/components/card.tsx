f21a2bb9652109728bab904421adc9327436e1ac
Nithin Monni <nithin.monni@gmail.com>
2025-02-09 07:11:21 -0600

    development qr

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -24,11 +24,11 @@ export default function Card(props: any) {
   return (
 
     <div className="flex w-full h-full flex-col content-center gap-2 pt-6 pb-6 text-center items-center justify-center">
-        <QRCode className="w-32 h-32" value={`http://localhost:3000${props.src}`} />
-        <p className="italic">{props.points} points</p>
-        <div className="text-center">
+      <QRCode className="w-32 h-32" value={process.env.NODE_ENV === "development" ? `http://localhost:3000${props.src}` : `https://socialchain-liart.vercel.app${props.src}`} />
+      <p className="italic">{props.points} points</p>
+      <div className="text-center">
         <button
-          
+
           className="px-4 py-1 bg-red-800 text-white border-2 border-black rounded-lg shadow-[4px_4px_0_0_#000] transition-all duration-300 hover:shadow-[2px_2px_0_0_#000] hover:translate-y-0.5"
         >
           Forfeit
