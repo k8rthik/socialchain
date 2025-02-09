4205c906f9d5fc965e9761e8585d092dec0bec58
Keerthik Muruganandam <murug030@umn.edu>
2025-02-08 21:36:31 -0600

    Added google auth

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { supabase } from '@/lib/supabase'; // Import the Supabase client
-import { assignRandomTasksToUser } from '../../utils/ass';
-import { useRouter } from 'next/navigation';
+import React, { useState } from "react";
+import { supabase } from "@/lib/supabase"; // Import the Supabase client
+import { assignRandomTasksToUser } from "../../utils/ass";
+import { useRouter } from "next/navigation";
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const router = useRouter();
@@ -27,11 +27,11 @@ const SignUp = () => {
 
       // Optionally, you can store the user in your own user table
       // Insert user data into a Supabase table
-      console.log('Sign up successful:', data);
-      router.push('/auth/login');
-      alert('Sign up successful, please check your email for verification!');
-    } catch (err : any) {
-      console.error('Error signing up:', err);
+      console.log("Sign up successful:", data);
+      router.push("/auth/login");
+      alert("Sign up successful, please check your email for verification!");
+    } catch (err: any) {
+      console.error("Error signing up:", err);
       setError(err.message);
     } finally {
       setLoading(false);
@@ -39,47 +39,62 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-900">
-      <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-96">
-        <h2 className="text-2xl font-semibold text-white mb-4">Sign Up</h2>
-        {error && <p className="text-red-500 text-sm">{error}</p>}
-        <form onSubmit={handleSignUp} className="space-y-4">
-          <div>
-            <label htmlFor="email" className="text-white">
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full px-4 py-2 bg-gray-700 text-white rounded-md"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="text-white">
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full px-4 py-2 bg-gray-700 text-white rounded-md"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            disabled={loading}
-            className={`w-full px-4 py-2 text-white bg-blue-500 rounded-md ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
-          >
-            {loading ? 'Signing Up...' : 'Sign Up'}
-          </button>
-        </form>
+    <div>
+      <div className="flex justify-center items-center h-screen bg-gray-900">
+        <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-96">
+          <h2 className="text-2xl font-semibold text-white mb-4">Sign Up</h2>
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+          <form onSubmit={handleSignUp} className="space-y-4">
+            <div>
+              <label htmlFor="email" className="text-white">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full px-4 py-2 bg-gray-700 text-white rounded-md"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="text-white">
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full px-4 py-2 bg-gray-700 text-white rounded-md"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={loading}
+              className={`w-full px-4 py-2 text-white bg-blue-500 rounded-md ${loading ? "opacity-50 cursor-not-allowed" : ""}`}
+            >
+              {loading ? "Signing Up..." : "Sign Up"}
+            </button>
+          </form>
+        </div>
+
+        <button
+          onClick={async () =>
+            await supabase.auth.signInWithOAuth({
+              provider: "google",
+              options: {
+                redirectTo: `@app/auth/callback/route.ts`,
+              },
+            })
+          }
+        >
+          Sign up with google
+        </button>
       </div>
     </div>
   );
